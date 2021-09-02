6d197d21c8aea11e1e18c5da460d75163b254d1e
masnursalim <nursalim.lsr@gmail.com>
2021-09-03 05:45:32 +0700

    belajar-go

---
+++ 18_strings/string_count.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Selamat malam madam"
+	fmt.Println(strings.Count(str,"ma"))
+}
---
+++ 18_strings/string_repeat.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+import "strings"
+
+func main() {
+	str := "Hello World"
+	fmt.Println(strings.Repeat(str, 4))
+}
